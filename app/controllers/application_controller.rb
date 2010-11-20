547e01fe49f79950959d5381708230ab532764f4
ounis ahmed <ounis.ahmed@gmail.com>
2010-11-20 19:41:35 +0100

    Fix CRLF

--- app/controllers/application_controller.rb
+++
@@ -1,8 +0,0 @@
-class ApplicationController < ActionController::Base
-  protect_from_forgery
-  rescue_from CanCan::AccessDenied do |exception|
-  	flash[:error] = "Access denied."
-  	redirect_to new_user_session_path
-  end
-  
-end
--- config/environment.rb
+++
@@ -1,6 +0,0 @@
-# Load the rails application
-require File.expand_path('../application', __FILE__)
-
-# Initialize the rails application
-GestionMagasin::Application.initialize!
-DEFAULT_CURRENCY_VALUES	= { :format => "%n %u", :unit => "DA", :separator => ".", :delimiter => " ", :precision => 2, :significant => false, :strip_insignificant_zeros => false }
