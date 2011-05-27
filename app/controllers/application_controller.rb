58c47ab04458c5d01e69a2782b451f2fcb05976d
unknown <ahmed@.(none)>
2011-05-27 17:18:45 +0100

    mise a jour 27/05/11

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   rescue_from CanCan::AccessDenied do |exception|
-  	flash[:error] = "Access denied."
-  	redirect_to new_user_session_path
-  end
-  
+    flash[:error] ="Access denied."
+    redirect_to new_user_session_path
+  end  
 end
